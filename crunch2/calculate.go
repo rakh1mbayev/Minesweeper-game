05c78e939eda6c5605e60b7e4811430c7c3da0a0
Bekzhan <114474251+rakh1mbayev@users.noreply.github.com>
2024-06-16 22:20:52 +0500

    Update calculate.go

--- crunch2/calculate.go
+++ crunch2/calculate.go
@@ -6,15 +6,13 @@ func calculate() {
 			if matrix[x][y] == '*' {
 				bombs_count++
 				postmatrix[x][y] = -1
-
 				for i := -1; i <= 1; i++ {
 					for j := -1; j <= 1; j++ {
-						if validation(x+i, y+j) && postmatrix[x+i][y+j] != '*' {
+						if validation(x+i, y+j) && postmatrix[x+i][y+j] != -1 {
 							postmatrix[x+i][y+j]++
 						}
 					}
 				}
-
 			}
 		}
 	}
